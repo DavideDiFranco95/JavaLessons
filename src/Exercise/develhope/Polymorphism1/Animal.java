885366617a92ab67274195a8e93208f6a70dcde8
DavideDiFranco95 <Davide.difranco_@outlook.it>
2023-01-18 15:49:05 +0100

    Esercizi Develhope

---
+++ src/Exercise/develhope/Polymorphism1/Animal.java
@@ -0,0 +1,14 @@
+package Exercise.develhope.Polymorphism1;
+
+public class Animal {
+    public String animalName;
+    public Animal(String animalName){
+        this.animalName=animalName;
+    }
+    public void animalSound(){
+
+    }
+    public void animalSound(String intensity){
+    }
+
+}
---
+++ src/Exercise/develhope/Polymorphism1/TesterClass.java
@@ -0,0 +1,9 @@
+package Exercise.develhope.Polymorphism1;
+
+public class TesterClass {
+    public static void main(String[] args) {
+        Animal lion= new Animal("Lion");
+        lion.animalSound();
+        lion.animalSound("Roarrrrrrr!");
+    }
+}
