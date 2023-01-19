7ef8050c131365a2c5220af75d7c1651b8f8abec
DavideDiFranco95 <Davide.difranco_@outlook.it>
2023-01-19 19:07:35 +0100

    Esercizi Develhope

---
+++ src/Exercise/develhope/InputAndOutput4/PrintYourNameAndPi.java
@@ -0,0 +1,9 @@
+package Exercise.develhope.InputAndOutput4;
+
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+        String name="Davide";
+        double numberPi=3.141590;
+        System.out.printf("%s %n %f",name.toUpperCase(),numberPi);
+    }
+}
