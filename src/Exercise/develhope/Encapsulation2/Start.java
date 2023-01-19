e3d46491a3f8cf5212756faaf8996ceacae03912
DavideDiFranco95 <Davide.difranco_@outlook.it>
2023-01-19 17:10:57 +0100

    Esercizi Develhope

---
+++ src/Exercise/develhope/Encapsulation2/Start.java
@@ -0,0 +1,18 @@
+package Exercise.develhope.Encapsulation2;
+
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+        Person person= new Person();
+        Scanner scanner= new Scanner(System.in);
+        String name=scanner.nextLine();
+        String surname=scanner.nextLine();
+        double height= scanner.nextDouble();
+        int age= scanner.nextInt();
+        System.out.printf("Name: %s ",name);
+        System.out.printf("Surname: %s ",surname);
+        System.out.printf("Height: %f ",height);
+        System.out.printf("Age: %d ",age);
+    }
+}
