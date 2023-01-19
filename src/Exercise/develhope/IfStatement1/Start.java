f6ca271830004f9c34a54635436fe7b15f7fe2ba
DavideDiFranco95 <Davide.difranco_@outlook.it>
2023-01-19 17:24:19 +0100

    Esercizi Develhope

---
+++ src/Exercise/develhope/IfStatement1/Start.java
@@ -0,0 +1,12 @@
+package Exercise.develhope.IfStatement1;
+
+public class Start {
+    public static void main(String[] args) {
+        String myName= "Davide";
+        if(myName.length() % 2 ==  0){
+            System.out.println("My name is Even. ");
+        }else{
+            System.out.println("my name is Odd.");
+        }
+    }
+}
--- src/Exercise/develhope/Polymorphism1/Animal.java
+++ src/Exercise/develhope/Polymorphism1/Animal.java
@@ -1,14 +1,18 @@
 package Exercise.develhope.Polymorphism1;
 
+import Exercise.develhope.Polymorphism2.Lion;
+
 public class Animal {
     public String animalName;
     public Animal(String animalName){
         this.animalName=animalName;
     }
     public void animalSound(){
+        System.out.println("Roarr!");
 
     }
     public void animalSound(String intensity){
+
     }
 
 }
