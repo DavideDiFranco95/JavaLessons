dcd36ca1d47dae2c0efc4d3d82f42e5efedef5d0
DavideDiFranco95 <Davide.difranco_@outlook.it>
2023-01-19 18:56:01 +0100

    Esercizi Develhope

---
+++ src/Exercise/develhope/InputAndOutput1/PrintYourName.java
@@ -0,0 +1,8 @@
+package Exercise.develhope.InputAndOutput1;
+
+public class PrintYourName {
+    public static void main(String[] args) {
+        String name="Davide";
+        System.out.println(name);
+    }
+}
