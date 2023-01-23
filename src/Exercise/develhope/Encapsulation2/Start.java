3b52f9a0773284faacb7136eda246d33ee020e42
DavideDiFranco95 <Davide.difranco_@outlook.it>
2023-01-23 18:50:10 +0100

    Esercizi Develhope

--- src/Exercise/develhope/Encapsulation2/Start.java
+++ src/Exercise/develhope/Encapsulation2/Start.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Start {
     public static void main(String[] args) {
-        Person person= new Person();
         Scanner scanner= new Scanner(System.in);
         String name=scanner.nextLine();
         String surname=scanner.nextLine();
