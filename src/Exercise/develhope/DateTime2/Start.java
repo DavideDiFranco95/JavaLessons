0464a7162bed9c21233bd4b8e93270e261c7fb2c
DavideDiFranco95 <Davide.difranco_@outlook.it>
2023-01-29 21:01:10 +0100

    Esercizi Develhope

--- src/Exercise/develhope/DateTime2/Start.java
+++ src/Exercise/develhope/DateTime2/Start.java
@@ -1,16 +1,22 @@
 package Exercise.develhope.DateTime2;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Start {
     public static void main(String[] args) {
-        LocalDateTime dateTime= LocalDateTime.now();
+        ZonedDateTime dateTime = ZonedDateTime.now();
+        ZoneId la = ZoneId.of("Europe/Rome");
+        ZonedDateTime date2 = dateTime.withZoneSameInstant(la);
 
 
         DateTimeFormatter f1 = DateTimeFormatter.ofPattern("dd-MMM-yy HH:mm");
-        DateTimeFormatter f2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm ");
+        DateTimeFormatter f2 = DateTimeFormatter.ofPattern("dd/MM/yyyy - hh:mm");
+
+
 
         System.out.println("The First Format is: " + dateTime.format(f1));
-        System.out.println("The Second Format is: " + dateTime.format(f2));
+        System.out.println("The Second Format is: " + date2.format(f2));
     }
 }
